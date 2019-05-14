a34e6b475713d92c2e71b24bf8d8dd7010c25c8d
Andy Bettisworth <andy@accreu.com>
2019-05-14 14:04:35 -0500

    Create react app

---
+++ todo/src/App.js
@@ -0,0 +1,31 @@
+'use strict'
+
+/**
+ * Dependencies
+ */
+
+const React = require('react')
+
+/**
+ * Import component styles
+ */
+
+require('./App.css')
+
+/**
+ * Define component
+ */
+
+function App() {
+  return (
+    <div>
+      App
+    </div>
+  )
+}
+
+/**
+ * Export component
+ */
+
+module.exports = App
